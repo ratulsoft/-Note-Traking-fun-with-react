3965b57bf1d7c8c65953b68f8ad17e80b8fed934
ratulsoft <mdratulbd1971@gmail.com>
2026-02-22 00:58:48 +0600

    add some feture

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,53 +2,88 @@ import { useState } from "react"
 
 
 function App() {
-  console.log("hello")
-  const [notetraking, setnotetraking] = useState("");  
-  const [note, setnote] = useState([]);
 
-   const notetrakingHandaler  = (e) => {
-     setnotetraking(e.target.value);
-     console.log(notetraking)
-   }
+    // create useState to hold data
+  const [notetraking, setnotetraking] = useState("");  
+  const [note, setnote] = useState([
+    {id:2,title:"ratul"},
+    {id:3,title:"yeasin"}
 
-   const submithandeler = (e)=> {
-    e.preventDefault()
-    if(notetraking.trim() === ""){
-      return alert("type eror")
-    }
-      const createNote = {
-        id:Date.now() + "" ,
-        title:notetraking,
+  ]);
+const[editmote , seteditmote] = useState(false);
+const[editable, seteditable] = useState(null); 
 
-      }
-      setnote([createNote, ...note]);
-     setnotetraking("")
-       
-  
-    }
-  return (
-    <>
-       <h1>Note Traking </h1>
-        <form onSubmit={submithandeler}>
-          <input type="text" value={notetraking} onChange={notetrakingHandaler} />
-          <button type="submit">Add Note</button>
-        </form>
-         <div className="note_list">
-           <h2> Note List Daily </h2>
-            <ul>
-            {note.map(note=>(
-              <li key={note}>
-                <span>{note.title}</span>
-                <button>Edit</button>
-                <button>Delete</button>
-              </li>
-           
-            ))}
-            </ul>
-            
-         </div>
-    </>
-  )
+  // track note 
+   const notetrakingHandaler  = (e) => {
+     setnotetraking(e.target.value);
+     console.log(notetraking)
+   }
+
+   const submithandeler = (e)=> {
+    e.preventDefault()
+    if(notetraking.trim() === ""){
+      return alert("type eror")
+    }
+      editmote ? updatehandaler() : createhandler()
+    }
+
+    const createhandler = ()=>{
+           const createNote = {
+        id:Date.now() + "" ,
+        title:notetraking,
+      }
+      setnote([createNote, ...note]);
+      setnotetraking("");
+    }
+
+
+function  edithandler (note){
+    seteditmote(true);
+    seteditable(note);
+    setnotetraking(note.title);
+}
+
+const updatehandaler = ()=>{
+    const update =  note.map((items)=>{
+       if(items.id === editable.id){
+        return {...items,title:notetraking }
+       } return items;
+    })
+ setnote(update)
+}
+
+
+// .........let's create delete handelear
+const deleteHandaler = (noteid)=>{
+const update = note.filter(items => items.id !== noteid );
+ setnote(update);
+} 
+
+
+
+  return (
+    <>
+       <h1>Note Traking </h1>
+        <form onSubmit={submithandeler}>
+          <input type="text" value={notetraking} onChange={notetrakingHandaler} />
+          <button type="submit">{editmote ?"update note":"add note"}</button>
+        </form>
+         <div className="note_list">
+           <h2> Note List Daily </h2>
+            <ul>
+            {note.map(note=>(
+              <li key={note}>
+                <span>{note.title}</span>
+                <button onClick={()=>edithandler(note)}>Edit</button>
+                <button onClick={()=>deleteHandaler(note.id)}>Delete</button>
+              </li>
+           
+            ))}
+            </ul>
+            
+         </div>
+    </>
+  )
 }
 
 export default App
